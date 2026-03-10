63d02dbd2c3dd915680fdc1165ec9f6f943ae6e3
ImJesusGomez <jesusgomez.dev10@gmail.com>
2026-03-10 17:21:40 -0600

    Manager and Location setup

--- src/regions/entities/region.entity.ts
+++ src/regions/entities/region.entity.ts
@@ -1,1 +1,13 @@
-export class Region {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Region {
+  @PrimaryGeneratedColumn('increment')
+  regionId: number;
+
+  @Column({ type: 'text', unique: true })
+  regionName: string;
+
+  @Column({ type: 'text', array: true })
+  regionState: string[];
+}
