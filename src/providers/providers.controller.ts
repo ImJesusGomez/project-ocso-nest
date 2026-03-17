8422a5eb351ae6a907c8d1d2078b1acc0da53f4c
ImJesusGomez <jesusgomez.dev10@gmail.com>
2026-03-17 15:43:06 -0600

    Auth for routes

--- src/providers/providers.controller.ts
+++ src/providers/providers.controller.ts
@@ -14,17 +14,19 @@ import { UpdateProviderDto } from './dto/update-provider.dto';
 import { UserData } from 'src/auth/decorators/user.decorator';
 import { User } from 'src/auth/entities/user.entity';
 import { Auth } from 'src/auth/decorators/auth.decorator';
+import { ROLES } from 'src/auth/constants/roles.constants';
 
 @Controller('providers')
 export class ProvidersController {
   constructor(private readonly providersService: ProvidersService) {}
 
+  @Auth(ROLES.MANAGER)
   @Post()
   create(@Body() createProviderDto: CreateProviderDto) {
     return this.providersService.create(createProviderDto);
   }
 
-  @Auth('Admin')
+  @Auth(ROLES.EMPLOYEE, ROLES.MANAGER)
   @Get()
   findAll(@UserData() user: User) {
     if (user.userRoles.includes('Employee'))
@@ -34,16 +36,19 @@ export class ProvidersController {
     return this.providersService.findAll();
   }
 
+  @Auth(ROLES.EMPLOYEE, ROLES.MANAGER)
   @Get('/name/:name')
   findOneByName(@Param('name') name: string) {
     return this.providersService.findOneByName(name);
   }
 
+  @Auth(ROLES.EMPLOYEE, ROLES.MANAGER)
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.providersService.findOne(id);
   }
 
+  @Auth(ROLES.MANAGER)
   @Patch(':id')
   update(
     @Param('id') id: string,
@@ -52,6 +57,7 @@ export class ProvidersController {
     return this.providersService.update(id, updateProviderDto);
   }
 
+  @Auth(ROLES.MANAGER)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.providersService.remove(id);
