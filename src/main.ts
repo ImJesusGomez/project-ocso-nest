75dcdf8b6448e5b2a60f521ed164cf4294296b99
ImJesusGomez <jesusgomez.dev10@gmail.com>
2026-04-18 11:27:37 -0600

    configuracion de cors.

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,11 @@ import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: process.env.ALLOWEDORIGIN,
+    },
+  });
   const config = new DocumentBuilder()
     .setTitle('Ocso API')
     .setDescription('API for ocso management')
@@ -20,6 +24,6 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
